7200b486710e73457829cc92ef266bbbe403bfb5
Philip Seeger <philip@c0xc.net>
2023-02-19 19:47:44 +0100

    Replace QProcess as its private buffer breaks status output and checks

--- inc/mainwindow.hpp
+++ inc/mainwindow.hpp
@@ -3,23 +3,28 @@
 ** Copyright (C) 2023 Philip Seeger <philip@c0xc.net>
 ** This file is part of UsbImgWriter.
 **
-** CapacityTester is free software: you can redistribute it and/or modify
+** UsbImgWriter is free software: you can redistribute it and/or modify
 ** it under the terms of the GNU General Public License as published by
 ** the Free Software Foundation, either version 3 of the License, or
 ** (at your option) any later version.
 **
-** CapacityTester is distributed in the hope that it will be useful,
+** UsbImgWriter is distributed in the hope that it will be useful,
 ** but WITHOUT ANY WARRANTY; without even the implied warranty of
 ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ** GNU General Public License for more details.
 **
 ** You should have received a copy of the GNU General Public License
-** along with CapacityTester. If not, see <http://www.gnu.org/licenses/>.
+** along with UsbImgWriter. If not, see <http://www.gnu.org/licenses/>.
 **
 ****************************************************************************/
 
 #ifndef MAINWINDOW_HPP
 #define MAINWINDOW_HPP
+
+#include <vector>
+#include <string>
+#include <unistd.h>
+#include <sys/wait.h>
 
 #include <QObject>
 #include <QVBoxLayout>
@@ -155,11 +160,19 @@ private:
     QString
     m_sel_drive;
 
-    QString
-    m_path_sudo;
-
 };
 
+/**
+ * FileWriter takes an input and an output stream,
+ * it reads all input data and writes each chunk of data to output in a loop.
+ *
+ * Originally written to also accept a QProcess instance (2nd ctor)
+ * in which case it writes to that process and sends status signals
+ * including bytesWritten(qint64).
+ * This almost works except that QProcess::write() actually writes
+ * to its internal writeBuffer which seems to have no limit (ex: 4GB RES).
+ */
+class ExtFileReaderWriter;
 class FileWriter : public QObject
 {
     Q_OBJECT
@@ -183,9 +196,20 @@ signals:
 
 public:
 
-    //FileWriter(QFileInfo input, QFileInfo output);
+    /**
+     * FileWriter can be initialized with two io devices,
+     * in which case it will just read from the first and write to the second.
+     * If initialized with a QProcess, it will call some extra methods.
+     *
+     * In any case, if the input or output object has no parent,
+     * FileWriter will become its parent,
+     * so it will take care of closing and deleting it.
+     */
     FileWriter(QIODevice *input, QIODevice *output);
     FileWriter(QIODevice *input, QProcess *output);
+    FileWriter(QIODevice *input, ExtFileReaderWriter *output);
+    //FileWriter(QIODevice *input, QFileInfo output); //convenience
+    //FileWriter(QFileInfo input, QIODevice *output); //convenience
 
 public slots:
 
@@ -194,10 +218,31 @@ public slots:
 
 private slots:
 
+    qint64
+    transfer();
+
+    void
+    confirmWritten(qint64 count);
+
+    void
+    finish();
+
+    void
+    confirmFinished();
+
     void
     handleFinished(int rc, QProcess::ExitStatus status);
 
 private:
+
+    qint64
+    m_read;
+
+    qint64
+    m_written;
+
+    qint64
+    m_auto_blocksize;
 
     bool
     m_done;
@@ -213,6 +258,84 @@ private:
 
     QProcess
     *m_out_proc;
+
+    ExtFileReaderWriter
+    *m_out_writer;
+
+};
+
+/**
+ * ExtFileReaderWriter takes a file path and provides an interface
+ * to write to that file (or read from it) through an external process.
+ * This external process would usually be dd.
+ * Without write permissions (not running as root),
+ * the process may be run through pkexec.
+ *
+ * In write mode (the default), the target file will be truncated
+ * if it's a regular file. But it shouldn't be:
+ * The target file is expected to be a storage device file like:
+ * /dev/sdd
+ *
+ * There's obviously a risk of some self-destruction involved.
+ * In the context of the UsbImgWriter program,
+ * the selected device will first be unmounted.
+ *
+ * TODO IDEA reimplement QIODevice - but without any hidden buffer
+ */
+class ExtFileReaderWriter : public QObject //TODO QIODevice
+{
+    Q_OBJECT
+
+signals:
+
+public:
+
+    ExtFileReaderWriter(QObject *parent = 0);
+    ExtFileReaderWriter(QString file, bool write = true, QObject *parent = 0);
+    ~ExtFileReaderWriter();
+
+    bool
+    needSudo();
+
+    QStringList
+    command();
+
+    void
+    start();
+
+    qint64
+    write(const char *data, qint64 max);
+
+    qint64
+    write(const QByteArray &bytes);
+
+    void
+    close();
+
+private:
+
+    void
+    openPipe();
+
+    void
+    closePipe();
+
+    std::vector<std::string>
+    cmd();
+
+    bool
+    m_write_mode;
+
+    int
+    m_pipefd[2];
+
+    QString
+    m_file;
+
+    int
+    m_pid;
+
+
 
 };
 
--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -11,6 +11,14 @@ MainWindow::MainWindow(QWidget *parent)
     setCentralWidget(wid_main);
     setWindowTitle(tr("USB IMG writer"));
 
+    //TODO planned feature ideas:
+    //- check format of input file
+    //- allow gzip compressed files to be loaded
+    //- add complex search&download dialog for Live Linux systems...
+    //  or for a custom image?
+    //- ask to create backup if device clicked before file selected
+    //  we'd use our ExtFileReaderWriter in read mode
+
     //Top layout
     QVBoxLayout *vbox = new QVBoxLayout;
     wid_main->setLayout(vbox);
@@ -111,7 +119,6 @@ MainWindow::MainWindow(QWidget *parent)
     vbox->addWidget(lbl_tux);
 
     //Misc
-    m_path_sudo = "pkexec";
 
 }
 
@@ -321,7 +328,7 @@ MainWindow::start()
     if (m_sel_drive.isEmpty()) return; //output drive selected
     if (!m_sel_drive.contains('/')) return;
 
-    //Open file
+    //Open input file
     QFile *file = new QFile(m_fi.filePath());
     if (!file->open(QIODevice::ReadOnly | QIODevice::Unbuffered))
     {
@@ -337,31 +344,23 @@ MainWindow::start()
     foreach (QPushButton *btn, m_all_buttons)
         btn->setDisabled(true);
 
+    //Create separate thread
+    QThread *thread = new QThread;
+
     //Use sudo equivalent if not running as root: pkexec
     //pkexec is our choice because gksu/kdesudo may not be installed by default
     //Note that this will show a password prompt (unless running as root),
     //which can be aborted.
 
     //Initialize write process
-    QProcess *proc = new QProcess;
-    QString wrapper;
-    QStringList cmd_arg;
-    bool need_sudo = !QFileInfo(m_sel_drive).isWritable();
-    bool debug_out_file = false;
-    if (need_sudo)
-        cmd_arg << m_path_sudo;
-    cmd_arg << "dd" << "bs=1M";
-    if (debug_out_file)
-        cmd_arg << QString("of=") + "/home/philip/tmp/TEST"; //debugging
-    else
-        cmd_arg << "of=" + m_sel_drive;
-    proc->setProgram(cmd_arg.takeFirst());
-    proc->setArguments(cmd_arg);
-    proc->setReadChannel(QProcess::StandardError);
+    //But not QProcess because we can't track its progress due to writeBuffer
+    //Initialize external file writer
+    QString out_file = m_sel_drive;
+    ExtFileReaderWriter *writer;
+    writer = new ExtFileReaderWriter(out_file); //could have thread as parent
 
-    //Create separate thread
-    QThread *thread = new QThread;
-    FileWriter *worker = new FileWriter(file, proc);
+    //Initialize our worker which will run in parallel
+    FileWriter *worker = new FileWriter(file, writer);
     worker->moveToThread(thread);
     //Hook up start/stop signals
     //https://doc.qt.io/qt-5/qthread.html#dtor.QThread
@@ -420,7 +419,7 @@ MainWindow::unblock()
 void
 MainWindow::handleDataWritten(qint64 count)
 {
-    qInfo() << count << "bytes written";
+    //qInfo() << count << "bytes written";
 
     //Handle error
     if (count <= 0)
@@ -518,29 +517,51 @@ MainWindow::progressedTux(double percentage)
 
 FileWriter::FileWriter(QIODevice *input, QIODevice *output)
           : QObject(),
+            m_read(0),
+            m_written(0),
+            m_auto_blocksize(0),
+            m_done(false),
+            m_failed(false),
             m_input(input),
             m_output(output),
             m_out_proc(0),
-            m_done(false),
-            m_failed(false)
+            m_out_writer(0)
 {
-    if (!m_input->parent()) m_input->setParent(this);
-    if (!m_output->parent()) m_output->setParent(this);
+    m_input = input;
+    m_output = output;
+    if (m_input)
+        if (!m_input->parent()) m_input->setParent(this);
+    if (m_output)
+    {
+        if (!m_output->parent()) m_output->setParent(this);
+        connect(m_output, SIGNAL(bytesWritten(qint64)), SLOT(confirmWritten(qint64)));
+    }
 
-    qInfo() << "worker initialized!";
+    if (m_input && m_output)
+        qInfo() << "worker initialized!";
 }
 
 FileWriter::FileWriter(QIODevice *input, QProcess *output)
           : FileWriter(input, (QIODevice*)output)
 {
-    qInfo() << "worker got control over external write process";
+    qInfo() << "worker got control over write qprocess";
 
     m_out_proc = output;
     connect(output, SIGNAL(finished(int, QProcess::ExitStatus)),
         SLOT(handleFinished(int, QProcess::ExitStatus)));
     connect(output, SIGNAL(started()), SLOT(run()));
 
-    output->start();
+    output->start(QIODevice::WriteOnly);
+}
+
+FileWriter::FileWriter(QIODevice *input, ExtFileReaderWriter *output)
+          : FileWriter(input, (QIODevice*)0)
+{
+    m_out_writer = output;
+    if (!m_out_writer->parent()) m_out_writer->setParent(this);
+    qInfo() << "worker got control over external writer";
+
+    QTimer::singleShot(0, this, SLOT(run()));
 }
 
 void
@@ -551,53 +572,162 @@ FileWriter::run()
         qInfo() << "writing to process" << m_out_proc->processId();
 
     //Read/write loop
+    int last_written = 0;
+    while (!m_failed && !m_done && last_written != -1)
+    {
+        last_written = transfer();
+    }
+
+}
+
+qint64
+FileWriter::transfer()
+{
+    //NOTE Qt's QFile and QProcess classes show a weird buffering behavior,
+    //they keep buffering all input from m_input to m_output
+    //though in blocks of 64KB (65535B internal write buffer)
+    //but that way, more than 100 MB of data are "written" in under a second,
+    //apparently stuck in yet another internal buffer somewhere in memory,
+    //without waiting for the process (to accept more data),
+    //rendering our logic useless as the GUI shows 100% progress,
+    //it stays there the whole time and after the fh's are closed,
+    //the data is actually written out, so we see no progress.
+    //QProcess has a method called waitForBytesWritten()
+    //but calling it (-1) does NOT help except for the very first block,
+    //from then on it once again fills whatever buffer with 64KB blocks
+    //until the whole file has been consumed before the output process
+    //has even committed 1%.
+    //Where are more than 100 MB of data buffered?
+    //It seems like it's all written to a private QByteArray
+    //which is hidden in some buffer struct called writeBuffer.
+    //And if we count the exact number of bytes written as received in
+    //confirmWritten() from the bytesWritten signal and
+    //only read/transfer the next block when ALL pending bytes have been
+    //confirmed/written, we get stuck at this point because
+    //there will be exactly 65535 bytes pending which are not committed,
+    //not confirmed written, so our confirmWritten() waits forever
+    //and the program just gets stuck in an awkward way.
+    //There does not seem to be a flush method in QProcess either...(?)
+    //So we determine the size of the write buffer in confirmWritten()
+    //by the number of bytes confirmed written in one go
+    //and assume that this is the maximum number of bytes
+    //that can get stuck in the buffer.
+    //With that, our confirmWritten() will know that if up to this number
+    //of bytes are not yet confirmed (i.e., > 0), it needs to call transfer
+    //to read more data for these currently buffered bytes to come out.
+    //So, our alternative to QProcess is ExtFileReaderWriter.
+
+    //Transfer (loop) iteration: read chunk of bytes, write them to m_output
     int bs = 1024 * 1024 * 1;
-    while (!m_failed)
+    if (m_failed) return -1;
     {
         //Read block
         QByteArray block = m_input->read(bs); //TODO handle read error
         if (block.isEmpty())
         {
             qInfo().noquote() << tr("empty read: done");
-            break;
+            m_done = true;
+            finish();
+            return 0;
         }
+        m_read += block.size();
         qInfo() << "read:" << block.size();
 
         //Write block
-        qint64 written = m_output->write(block);
+        qint64 written;
+        if (m_output)
+            written = m_output->write(block);
+        else if (m_out_writer)
+            written = m_out_writer->write(block);
         if (written != block.size())
         {
             qInfo().noquote() << tr("write failed: %1 written (out of %2)").arg(written).arg(block.size());
             m_failed = true;
             emit writeFailed();
-            return;
-        }
-        if (m_out_proc)
-        {
-            m_out_proc->waitForBytesWritten(-1); //TODO count ALL bytes written
+            return -1;
         }
         qInfo() << "written:" << written;
+        if (m_out_proc)
+        {
+            //QProcess is our target - it buffers internally without blocking
+            //This should wait/block until all data are written/synced
+            //...but it doesn't.
+            qInfo() << "waiting for output process...";
+            bool ok = m_out_proc->waitForBytesWritten(-1);
+            qInfo() << "output process confirmed bytes written" << ok;
+        }
 
         emit bytesWritten(written);
+        return written;
     }
-    if (m_failed) return;
+
+    //Next iteration called by slot after bytesWritten()
+}
+
+void
+FileWriter::confirmWritten(qint64 count)
+{
+    //UNUSED due to Qt's private writeBuffer (and count grows: 54K, 128K...)
+}
+
+/**
+ * finish() will close the file handles and schedule the finished signal
+ * to be emitted, except if an error has occurred in the meantime.
+ *
+ * The finished signal is not emitted immediately because
+ * m_output might be a QProcess which can fail
+ * after waitForFinished() returned, in the handleFinished() slot.
+ */
+void
+FileWriter::finish()
+{
+    if (m_failed || !m_done) return;
 
     //Close pipe
-    m_done = true;
     m_input->close();
     if (m_out_proc)
     {
+        //Wait for QProcess to finish writing
+        //Due to the nature of QProcess and its internal buffer,
+        //trying to write a file that's 4x larger than the target device
+        //may still fail after waitForFinished() returned,
+        //in the handleFinished() slot.
+        //So that slot will send the finished signal.
         qInfo() << "closing pipe and waiting for process" << m_out_proc->processId();
         m_out_proc->closeWriteChannel();
-        m_out_proc->waitForFinished(-1); //If msecs is -1, this function will not time out.
+        m_out_proc->waitForFinished(-1); //"If msecs is -1, this function will not time out."
         qInfo() << "done waiting for process";
     }
-    else
+    else if (m_out_writer)
+    {
+        m_out_writer->close();
+    }
+    else if (m_output)
     {
         m_output->close();
     }
 
-    //Confirm process finished, this may be deleted now
+    //Confirm process finished + this may be deleted
+    //Confirm that we're done now, except if QProcess is involved,
+    //in which case this happens in the handleFinished slot called
+    //when QProcess itself is finished.
+    //Otherwise, we'd have a race condition causing
+    //a message to be shown to the user confirming success
+    //before QProcess actually sends all the data to the process
+    //which then fails.
+    if (!m_out_proc)
+    {
+        confirmFinished();
+    }
+}
+
+void
+FileWriter::confirmFinished()
+{
+    //Do not confirm if an error has occurred (error signal already sent)
+    if (m_failed || !m_done) return;
+
+    //Now finally confirm that we're done
     emit finished();
 }
 
@@ -626,5 +756,167 @@ FileWriter::handleFinished(int rc, QProcess::ExitStatus status)
         return;
     }
 
+    //Send finished signal to confirm we're done
+    //(in other cases, i.e., if not writing to QProcess, this was already done)
+    confirmFinished();
+}
+
+ExtFileReaderWriter::ExtFileReaderWriter(QObject *parent)
+                   : QObject(parent),
+                     m_write_mode(true),
+                     m_pid(0)
+{
+    openPipe();
+}
+
+ExtFileReaderWriter::ExtFileReaderWriter(QString file, bool write, QObject *parent)
+                   : ExtFileReaderWriter(parent)
+{
+    m_file = file;
+
+    start();
+
+}
+
+ExtFileReaderWriter::~ExtFileReaderWriter()
+{
+    //Close write pipeline
+    closePipe();
+
+    //Wait for child process to finish
+    if (m_pid)
+    {
+        int wstatus;
+        waitpid(m_pid, &wstatus, 0);
+    }
+
+}
+
+bool
+ExtFileReaderWriter::needSudo()
+{
+    bool need_sudo = !QFileInfo(m_file).isWritable();
+    return need_sudo;
+}
+
+QStringList
+ExtFileReaderWriter::command()
+{
+
+    //Check for special characters which may cause harm if run in a shell
+    //(even though we don't...)
+    if (m_file.contains(' ') || m_file.contains('"') || m_file.contains("'"))
+        throw std::runtime_error("illegal file name");
+
+    //Construct command: dd for writing; pkexec to gain root privileges
+    //pkexec is preferred over kdesu/gksu which may not be preinstalled
+    QStringList cmd_list;
+    cmd_list << "dd";
+    if (needSudo())
+        cmd_list.prepend("pkexec"); //PolicyKit will show a password prompt
+    cmd_list << "bs=4";
+    //write or read mode - write by default
+    if (m_write_mode)
+        cmd_list << QString("of=") + m_file;
+    else
+        cmd_list << QString("if=") + m_file;
+
+    return cmd_list;
+}
+
+std::vector<std::string>
+ExtFileReaderWriter::cmd()
+{
+    std::vector<std::string> args_v;
+    foreach (QString a, this->command())
+        args_v.push_back(a.toLocal8Bit().constData());
+
+    return args_v;
+}
+
+void
+ExtFileReaderWriter::start()
+{
+    //Define external call
+    std::vector<std::string> args_v = this->cmd();
+
+    //Fork child process
+    qInfo() << "ExtFileReaderWriter forking...";
+    int p = fork();
+    if (p == -1)
+        throw std::runtime_error("failed to fork");
+    if (p == 0)
+    {
+        //Connect stdin to write pipe of parent
+        dup2(m_pipefd[0], STDIN_FILENO);
+        closePipe();
+
+        //Prepare arguments for exec call
+        const char *prog = args_v[0].c_str();
+        char **args = new char*[args_v.size() + 1]; //for (std::string s: args_v)
+        for (int i = 0; i < (int)args_v.size(); i++)
+        {
+            char *str = args[i] = new char[args_v[i].length() + 1];
+            strcpy(str, args_v[i].c_str());
+        }
+        args[args_v.size()] = 0;
+        //Call external program
+        qInfo() << "ExtFileReaderWriter calling external program" << QString(prog);
+        qInfo() << QString(prog) << args[0];
+        execvp(prog, const_cast<char* const*>(args));
+        throw std::runtime_error("execvp failed");
+
+        return;
+    }
+    else
+    {
+        qInfo() << "ExtFileReaderWriter working with" << p;
+        m_pid = p;
+    }
+}
+
+qint64
+ExtFileReaderWriter::write(const char *data, qint64 max)
+{
+    return ::write(m_pipefd[1], data, max);
+    //if written < max && written > 0:
+    const char *dataptr = data;
+    qint64 total = 0;
+    while (true)
+    {
+        qint64 count = ::write(m_pipefd[1], dataptr, max);
+        if (count == -1) return count;
+        total += count;
+        dataptr = dataptr + count;
+    }
+    return total;
+}
+
+qint64
+ExtFileReaderWriter::write(const QByteArray &bytes)
+{
+    return write(bytes.constData(), bytes.size());
+}
+
+void
+ExtFileReaderWriter::close()
+{
+    closePipe();
+}
+
+void
+ExtFileReaderWriter::openPipe()
+{
+    if (pipe(m_pipefd) == -1)
+        throw std::runtime_error("pipe failed");
+}
+
+void
+ExtFileReaderWriter::closePipe()
+{
+    ::close(m_pipefd[0]);
+    ::close(m_pipefd[1]);
+    m_pipefd[0] = 0;
+    m_pipefd[1] = 0;
 }
 
